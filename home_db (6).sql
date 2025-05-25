159d5b499eb571edcb9263f946904514f432526a
Yuvaraj Maji <166051571+raj-maji@users.noreply.github.com>
2025-05-25 19:46:04 +0530

    Add files via upload

---
+++ home_db (6).sql
@@ -0,0 +1,214 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 12, 2025 at 05:30 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `home_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `name`, `password`) VALUES
+('BcjKNX58e4x7bIqIvxG7', 'SumanMukherjee', 'b024a41504072024b630d56d626e63531df89b58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `number` varchar(10) NOT NULL,
+  `message` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `name`, `email`, `number`, `message`) VALUES
+('Ae6FGXPFN9TUUFqnZ8Ox', 'SumanMukherjee', 'sumanmukherjee6294@gmail.com', '6294458325', 'I face a problem');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `property`
+--
+
+CREATE TABLE `property` (
+  `id` varchar(20) NOT NULL,
+  `user_id` varchar(20) NOT NULL,
+  `property_name` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `price` varchar(10) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `furnished` varchar(50) NOT NULL,
+  `bhk` varchar(10) NOT NULL,
+  `deposite` varchar(10) NOT NULL,
+  `bedroom` varchar(10) NOT NULL,
+  `bathroom` varchar(10) NOT NULL,
+  `balcony` varchar(10) NOT NULL,
+  `carpet` varchar(10) NOT NULL,
+  `age` varchar(2) NOT NULL,
+  `total_floors` varchar(2) NOT NULL,
+  `room_floor` varchar(2) NOT NULL,
+  `loan` varchar(50) NOT NULL,
+  `lift` varchar(3) NOT NULL DEFAULT 'no',
+  `security_guard` varchar(3) NOT NULL DEFAULT 'no',
+  `play_ground` varchar(3) NOT NULL DEFAULT 'no',
+  `garden` varchar(3) NOT NULL DEFAULT 'no',
+  `water_supply` varchar(3) NOT NULL DEFAULT 'no',
+  `power_backup` varchar(3) NOT NULL DEFAULT 'no',
+  `parking_area` varchar(3) NOT NULL DEFAULT 'no',
+  `gym` varchar(3) NOT NULL DEFAULT 'no',
+  `shopping_mall` varchar(3) NOT NULL DEFAULT 'no',
+  `hospital` varchar(3) NOT NULL DEFAULT 'no',
+  `school` varchar(3) NOT NULL DEFAULT 'no',
+  `market_area` varchar(3) NOT NULL DEFAULT 'no',
+  `image_01` varchar(50) NOT NULL,
+  `image_02` varchar(50) NOT NULL,
+  `image_03` varchar(50) NOT NULL,
+  `image_04` varchar(50) NOT NULL,
+  `image_05` varchar(50) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp(),
+  `video_01` varchar(700) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `property`
+--
+
+INSERT INTO `property` (`id`, `user_id`, `property_name`, `address`, `price`, `type`, `status`, `furnished`, `bhk`, `deposite`, `bedroom`, `bathroom`, `balcony`, `carpet`, `age`, `total_floors`, `room_floor`, `loan`, `lift`, `security_guard`, `play_ground`, `garden`, `water_supply`, `power_backup`, `parking_area`, `gym`, `shopping_mall`, `hospital`, `school`, `market_area`, `image_01`, `image_02`, `image_03`, `image_04`, `image_05`, `description`, `date`, `video_01`) VALUES
+('Y8I99RT2issIkq7P2vun', 'g7KQOKnZVW7lz36UdulJ', 'Rental House ', 'Barasat Medical college(District Hospital).', '6000', 'flat', 'ready to move', 'furnished', '1', '2000', '1', '1', '0', '122', '10', '5', '2', '', 'yes', 'yes', 'yes', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'no', 'Y5PBVuQN2zW7T94d1WcA.png', 'Lpspoi1utHb6Go5yk0x3.jpg', 'YPnWjVfaOHbjp2YIu6Zx.jpg', 'zdT3FSi74fFewXhmapyQ.jpg', '', 'good', '2025-05-12', 'R3gtuj9C2lUFtXZfGQKl.mp4');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requests`
+--
+
+CREATE TABLE `requests` (
+  `id` varchar(20) NOT NULL,
+  `property_id` varchar(20) NOT NULL,
+  `sender` varchar(20) NOT NULL,
+  `receiver` varchar(20) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `rating` float NOT NULL,
+  `feedback` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `name`, `rating`, `feedback`, `created_at`) VALUES
+(1, 'SumanMukherjee', 5, 'good', '2025-05-11 17:51:51'),
+(2, 'yuvaraj', 4.5, 'abcd', '2025-05-11 18:07:23'),
+(3, 'admin', 3.5, 'hhhhhh', '2025-05-11 18:11:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `saved`
+--
+
+CREATE TABLE `saved` (
+  `id` varchar(20) NOT NULL,
+  `property_id` varchar(20) NOT NULL,
+  `user_id` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `number` varchar(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `image1` varchar(50) NOT NULL,
+  `image2` varchar(50) NOT NULL,
+  `upi_id` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `number`, `email`, `password`, `image1`, `image2`, `upi_id`) VALUES
+('g7KQOKnZVW7lz36UdulJ', 'SumanMukherjee', '6294458325', 'sumanmukherjee62@gmail.com', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'key for screenshot in windows 11 - Google Search -', 'key for screenshot in windows 11 - Google Search -', 'sumanmukherjee6294-1@oksbi');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
